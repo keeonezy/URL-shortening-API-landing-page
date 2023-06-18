8f2bc2f393a49f4a99ec5b14d4e2d233b20149a9
Alexander Malukhin <keeonenew@yandex.ru>
2023-06-18 16:25:24 +0300

    refactor test version

--- scripts/index.js
+++ scripts/index.js
@@ -16,7 +16,7 @@ async function getShortUrl() {
     const newShort = document.createElement("div");
     newShort.classList.add('shorts__result');
     newShort.innerHTML = `
-    <p class="boost__title" style="color: black;">${data.result.original_link}</p>
+    <p class="fwe" style="color: black; font-size: 18px;">${data.result.original_link}</p>
     <input class="shorts__result-input" value="" readonly type="text">
       <button class="shorts__result-button">Скопировать</button>
       `;
@@ -24,16 +24,14 @@ async function getShortUrl() {
     // метод appendChild работает не корректно. не добавляет новые input при множестве
     resultContainer.prepend(newShort);
 
+
     const resultButton = document.querySelector(".shorts__result-button");
     const resultText = document.querySelector(".shorts__result-input");
     resultText.value = data.result.short_link;
-    resultButton.addEventListener("click", copyResult);
-
-    function copyResult(e) {
+    resultButton.addEventListener("click", (e) => {
         e.preventDefault();
-        navigator.clipboard.writeText(resultText.textContent);
-    }
-
+        navigator.clipboard.writeText(resultText.value);
+    });
 
     searchInput.value = "";
 }
@@ -44,3 +42,16 @@ function goResult(e) {
 }
 
 searchButton.addEventListener("click", goResult);
+
+
+// function setLocalStorage() {
+//     localStorage.setItem("adviceCounter", inputSearch.value);
+// }
+// window.addEventListener("beforeunload", setLocalStorage);
+
+// function getLocalStorage() {
+//     if (localStorage.getItem("adviceCounter")) {
+//         inputSearch.value = localStorage.getItem("adviceCounter");
+//     }
+// }
+// window.addEventListener("load", getLocalStorage);
