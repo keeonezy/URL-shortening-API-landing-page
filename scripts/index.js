212144187610b8febd767754ba7b557467afbc65
Alexander Malukhin <keeonenew@yandex.ru>
2023-06-18 13:25:55 +0300

    feat: add js short ul

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const searchInput = document.querySelector(".shorts__input");
+const searchButton = document.querySelector(".shorts__button");
+const resultShort = document.querySelector(".shorts__text");
+const resultContainer = document.querySelector(".shorts__result");
+
+async function getShortUrl() {
+    const url = `https://api.shrtco.de/v2/shorten?url=${searchInput.value}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    resultShort.textContent = data.result.full_short_link;
+    searchInput.value = "";
+    resultContainer.style.display = "block";
+
+}
+
+function goResult(e) {
+    e.preventDefault();
+    getShortUrl();
+}
+
+searchButton.addEventListener("click", goResult);
